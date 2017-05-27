b01fc51defe26311d52e0c507ae74f502339d3a3
Tim Cash <timcash@gmail.com>
2017-05-27 13:20:23 -0700

    zoom in and out

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const gd = require('node-gd')
-const chroma = require('chroma-js')
+//const gd = require('node-gd')
+//const chroma = require('chroma-js')
 const burntOrange = '#892513'
 const orangeBrown = '#AC4315'
 const yellowOrange = '#F0BB64'
@@ -11,33 +11,34 @@ const chocolate = '#D2691E'
 const gold = '#F8DC57'
 const black = 'black'
 const white = 'white'
+//const colors = [chocolate,yellowOrange,lightTeal,deepOcean]
 //const colors = [deepBlue,gold,burntOrange,gold,deepBlue]
-//const C_SCALE = chroma.scale('Spectral').domain([1,0])
+const C_SCALE = chroma.scale('Spectral').domain([1,0])
 //const C_SCALE = chroma.bezier(colors);
 //const C_SCALE = chroma.scale(colors.reverse())
-const colors = [chocolate,yellowOrange,lightTeal,deepOcean]
-const C_SCALE = chroma.scale(colors.reverse())
+//const C_SCALE = chroma.scale(colors.reverse())
 let pi2 = 2 * Math.PI
 // const CANVAS_HEIGHT = 10 * 36
 // const CANVAS_WIDTH = 10 * 24
 const CANVAS_HEIGHT = 72 * 2 * 36
 const CANVAS_WIDTH = 72 * 2 * 24
 const MAX_ITERS = 1500
-const TOTAL_PX = CANVAS_WIDTH * CANVAS_HEIGHT
-const RATIO = CANVAS_HEIGHT / CANVAS_WIDTH
+
 const ln = Math.log
 const floor = Math.floor
 const ln2 = ln(2)
 const juliaA = -0.8
 const juliaB = 0.156
+
+// center
 // let CENTER = [0,0]
-// let ZOOM = 2
+// let ZOOM = 3
 
 // fish
 // let ZOOM = 0.0008523688595400003
 // let CENTER = [0.36330877028148983, -0.31721691316595024]
 
-// Portal
+//Portal
 // let ZOOM = 0.00009282296880390604
 // let CENTER = [-0.7527003524317856, -0.04307864384625851]
 
@@ -46,8 +47,8 @@ const juliaB = 0.156
 // let CENTER = [-0.7527583370839379, -0.0430666696832828]
 
 // Hook
-let ZOOM = 0.0025829359380000008
-let CENTER = [0.2872320833755557, -0.012069614516666667]
+// let ZOOM = 0.0025829359380000008
+// let CENTER = [0.2872320833755557, -0.012069614516666667]
 
 // lightning
 // let ZOOM = 0.007827078600000002
@@ -57,13 +58,89 @@ let CENTER = [0.2872320833755557, -0.012069614516666667]
 // let ZOOM = 0.000055864611
 // let CENTER = [-0.7436429465554844, -0.13182541971477443]
 
-function loopMandle(c) {
+// const ZOOM = 0.0000034915381875
+// const CENTER = [-0.7436429465554844,-0.13182541971477443]
+
+// const ZOOM = 0.0000020690596666666664
+// const CENTER = [-0.7436429465554844,-0.13182452982598443]
+
+//julia set
+const ZOOM = 0.000009003668544858957
+const CENTER = [-1.7692335805601607,-0.0034126996797283586]
+
+// const ZOOM = 1.6192669975228917e-8
+// const CENTER = [-1.769233646731526,-0.0034129183486770616]
+
+// const ZOOM = 4.46205213837408e-8
+// const CENTER = [-1.7692336402519278,-0.003412913924479787]
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+const WIDTH = 1280
+const HEIGHT = Math.floor(WIDTH * 0.75)
+makeMandles(WIDTH, HEIGHT)
+
+function makeMandles (width, height) {
+  for(let i = 0; i < 1; i++) {
+    const canvasElement = document.createElement('canvas')
+    canvasElement.id = `canvas${i}`
+    document.body.appendChild(canvasElement)
+    const canvas = document.getElementById(`canvas${i}`)
+    const ctx = canvas.getContext('2d')
+    ctx.canvas.width = width
+    ctx.canvas.height = height
+    ctx.fillStyle = 'green'
+    ctx.fillRect(0, 0, width, height)
+
+    let center = CENTER
+    let zoom = ZOOM / Math.pow(2, i)
+    const textElement = document.createElement('div')
+    textElement.innerHTML = `const ZOOM = ${zoom}</br> const CENTER = [${CENTER}]`
+    document.body.appendChild(textElement)
+    canvas.onmousedown = e => {
+
+      console.log(`let ZOOM = ${zoom}\nlet CENTER = [${e.layerX}, ${e.layerY}]`)
+      const [a,b] = toComplexAtCenterAndZoom(e.layerX, e.layerY, width, height, center, zoom, width / height)
+      if(e.button === 0) zoom = zoom * 0.80
+      if(e.button === 2) zoom = zoom * 1.33
+      center = [a,b]
+      console.log(`let ZOOM = ${zoom}\nlet CENTER = [${a}, ${b}]`)
+      textElement.innerHTML = `const ZOOM = ${zoom}</br> const CENTER = [${center}]`
+      draw(i, ctx, width, height, center, zoom, C_SCALE, MAX_ITERS)
+    }
+    draw(i, ctx, width, height, center, zoom, C_SCALE, MAX_ITERS)
+  }
+}
+
+function draw (id, ctx, width, height, center, zoom, colorScale, maxIters) {
+  const totalPx = width * height
+  const textElement = document.getElementById(`canvas${id}`)
+  const [temps, histo] = buildMandleData(width, height, center, zoom, maxIters)
+  const histoedData = histoTheData(temps, histo, totalPx, maxIters)
+  setMandleImageData(ctx, width, height, temps, histoedData, colorScale, maxIters)
+}
+
+function setMandleImageData(ctx, width, height, temps, histoedData, cScale, maxIters) {
+  let myImageData = ctx.getImageData(0,0, width, height)
+  let data = myImageData.data;
+  for (let j = 0; j < histoedData.length; j += 1) {
+    const iters = temps[j]
+    const temp = histoedData[j]
+    let rgb = cScale(temp).rgb()
+    if (iters === maxIters) rgb = [0, 0, 0]
+    data[j * 4]     = rgb[0];     // red
+    data[j * 4 + 1] = rgb[1]; // green
+    data[j * 4 + 2] = rgb[2]; // blue
+  }
+  ctx.putImageData(myImageData, 0, 0);
+}
+
+function loopMandle(c, maxIters) {
   let x0 = c[0]
   let y0 = c[1]
   let x = x0
   let y = y0
   let iteration = 0
-  while (x * x + y * y < (1 << 16) && iteration < MAX_ITERS) {
+  while (x * x + y * y < (1 << 16) && iteration < maxIters) {
     let xtemp = x * x - y * y + x0
     y = 2 * x * y + y0
     // let xtemp = x * x - y * y + juliaA
@@ -72,7 +149,7 @@ function loopMandle(c) {
     iteration += 1
   }
 
-  if (iteration < MAX_ITERS) {
+  if (iteration < maxIters) {
     const log_zn = ln(x * x + y * y) / 2
     const nu = ln(log_zn / ln2) / ln2
     iteration = iteration + 1 - nu
@@ -80,19 +157,20 @@ function loopMandle(c) {
   return iteration
 }
 
-function buildMandleData(width, height, center, zoom) {
+function buildMandleData(width, height, center, zoom, maxIters) {
   let mData = []
   let histo = []
   let counter = 0
+  const ratio = width / height
 
-  for (let i = 0; i < MAX_ITERS; i++) {
+  for (let i = 0; i < maxIters; i++) {
     histo.push(0)
   }
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
-      const [a, b] = toComplexAtCenterAndZoom(x,y, width, height, center, zoom)
-      const loops = loopMandle([a, b])
-      if (counter % 1000 === 0) console.log('Index', counter)
+      const [a, b] = toComplexAtCenterAndZoom(x,y, width, height, center, zoom, ratio)
+      const loops = loopMandle([a, b], maxIters)
+      //if (counter % 1000 === 0) console.log('Index', counter)
       histo[floor(loops)] += 1
       mData.push(loops)
       counter += 1
@@ -101,12 +179,12 @@ function buildMandleData(width, height, center, zoom) {
   return [mData, histo]
 }
 
-function histoTheData(temps, histo) {
+function histoTheData(temps, histo, totalPx, maxIters) {
   let palette = []
   let hues = []
-  palette[0] = histo[0] / TOTAL_PX
-  for (let i = 1; i < MAX_ITERS; i++) {
-    palette[i] = palette[i - 1] + histo[i] / TOTAL_PX
+  palette[0] = histo[0] / totalPx
+  for (let i = 1; i < maxIters; i++) {
+    palette[i] = palette[i - 1] + histo[i] / totalPx
   }
   for (let i = 0; i < temps.length; i++) {
     const t = temps[i]
@@ -117,32 +195,17 @@ function histoTheData(temps, histo) {
   return hues
 }
 
-function draw() {
-  const [temps, histo] = buildMandleData(CANVAS_WIDTH, CANVAS_HEIGHT, CENTER, ZOOM)
-  const histoedData = histoTheData(temps, histo)
+// window.onmousedown = (e) => {
+//   const [a,b] = toComplexAtCenterAndZoom(e.clientX,e.clientY,CANVAS_WIDTH, CANVAS_HEIGHT, CENTER,ZOOM)
+//   ZOOM = ZOOM * 0.33
+//   CENTER = [a,b]
+//   console.log(`let ZOOM = ${ZOOM}\nlet CENTER = [${a}, ${b}]`)
+//   draw()
+// }
 
-  gd.createTrueColor(CANVAS_WIDTH, CANVAS_HEIGHT, function(error, image) {
-    if (error) throw error;
-    for (let i = 0; i < histoedData.length; i ++) {
-      const iters = temps[i]
-      const temp = histoedData[i]
-      let rgb = C_SCALE(temp).rgb()
-      if (iters === MAX_ITERS) rgb = [0, 0, 0]
-      const col = i % CANVAS_WIDTH
-      const row = Math.floor(i / CANVAS_WIDTH)
-      image.setPixel(col, row, gd.trueColor(rgb[0], rgb[1],rgb[2]))
-    }
-
-    image.savePng('./test.png', 0, function(error) {
-      if (error) throw error;
-      image.destroy();
-    })
-  });
-}
-
-function toComplexAtCenterAndZoom(x,y,w,h,center,zoom) {
-  let top = center[1] - (zoom * RATIO)
-  let bottom = center[1] + (zoom * RATIO)
+function toComplexAtCenterAndZoom(x,y,w,h,center,zoom, ratio) {
+  let top = center[1] - (zoom * ratio)
+  let bottom = center[1] + (zoom * ratio)
   let left = center[0] - zoom
   let right = center[0] + zoom
   const a = numberMap(x, 0, w, left, right)
@@ -180,9 +243,8 @@ function lerp(a, b, f) {
 }
 
 function numberMap(i, min, max, smin, smax) {
-  const p = (i + min) / max
-  const sDist = smax - smin
-  return p * sDist + smin
+  const oDist = i - min
+  const oMaxDist = max - min
+  const p = oDist / oMaxDist
+  return lerp(smin, smax, p)
 }
-
-draw()
