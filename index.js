3c3a3c89d7be49c2f3a351ab3a32b6629d657893
Tim Cash <timcash@gmail.com>
2017-12-18 20:29:38 -0800

    Not sure

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-const gd = require('node-gd')
-const chroma = require('chroma-js')
-const burntOrange = '#892513'
-const orangeBrown = '#AC4315'
-const yellowOrange = '#F0BB64'
-const lightTeal = '#EFFCFD'
-const deepBlue = '#0C277E'
-const deepOcean = '#001B5B'
-const lightBlue = '#B7D7F0'
-const chocolate = '#D2691E'
-const gold = '#F8DC57'
-const black = 'black'
-const white = 'white'
+const gd = require("node-gd")
+const chroma = require("chroma-js")
+const color_theme = require("nice-color-palettes")
+
+const burntOrange = "#892513"
+const orangeBrown = "#AC4315"
+const yellowOrange = "#F0BB64"
+const lightTeal = "#EFFCFD"
+const deepBlue = "#0C277E"
+const deepOcean = "#001B5B"
+const lightBlue = "#B7D7F0"
+const chocolate = "#D2691E"
+const gold = "#F8DC57"
+const black = "black"
+const white = "white"
 
 let pi2 = 2 * Math.PI
 const DPI = 72 * 2
-// const CANVAS_HEIGHT = DPI * 36
-// const CANVAS_WIDTH = DPI * 24
-const CANVAS_HEIGHT = DPI * 9
-const CANVAS_WIDTH = DPI * 6
+const COLOR_SHIFTS = 15
+const DOMAINS = [0, 0.25, 0.5, 0.6, 0.7, 0.75, 0.8, 0.82, 0.85, 0.87, 0.91, 0.92, 0.95, 0.99, 1]
+// const COLOR_SHIFTS = 6
+// const DOMAINS = [0, 0.25, 0.5, 0.6, 0.7, 1]
+// const COLOR_SHIFTS = 7
+// const DOMAINS = [0, 0.25, 0.5, 0.6, 0.7, 0.85, 1]
+// const COLOR_SHIFTS = 9
+// const DOMAINS = [0, 0.25, 0.5, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
+const CANVAS_HEIGHT = DPI * 36
+const CANVAS_WIDTH = DPI * 24
+// const CANVAS_HEIGHT = DPI * 9
+// const CANVAS_WIDTH = DPI * 6
 const MAX_ITERS = 10 * 1000
 const TOTAL_PX = CANVAS_WIDTH * CANVAS_HEIGHT
 const RATIO = CANVAS_HEIGHT / CANVAS_WIDTH
@@ -31,16 +41,17 @@ const ln2 = ln(2)
 
 // julia
 // const ZOOM = 0.000006204954835888166
-const ZOOM = 0.000006202934835887166
-const CENTER = [-1.7692336509013211,-0.0034128997612515775]
+// const ZOOM = 0.000006202934835887166
+// const CENTER = [-1.7692336509013211,-0.0034128997612515775]
 
 // fish
 // let ZOOM = 0.0008523688595400003
 // let CENTER = [0.36330877028148983, -0.31721691316595024]
 //
 //Portal
-// let ZOOM = 0.00009282496880390604
-// let CENTER = [-0.7527003524317856, -0.04307864384625851]
+//let ZOOM = 0.00009282496880390604
+let ZOOM = 0.00012982499880390604
+let CENTER = [-0.7527003524317856, -0.04307864384625851]
 //
 // Desert Oasis
 // let ZOOM = 0.000010108421302745368
@@ -64,7 +75,7 @@ const CENTER = [-1.7692336509013211,-0.0034128997612515775]
 
 // long julia forest
 // const ZOOM = 6.014888640493153e-11
-// const CENTER = [0.35245792654678254,-0.5823515765137375]
+// const CENTER = [0.35245792654678254, -0.5823515765137375]
 
 function loopMandle(c) {
   let x0 = c[0]
@@ -72,7 +83,7 @@ function loopMandle(c) {
   let x = x0
   let y = y0
   let iteration = 0
-  while (x * x + y * y < (1 << 16) && iteration < MAX_ITERS) {
+  while (x * x + y * y < 1 << 16 && iteration < MAX_ITERS) {
     let xtemp = x * x - y * y + x0
     y = 2 * x * y + y0
     // let xtemp = x * x - y * y + juliaA
@@ -99,9 +110,9 @@ function buildMandleData(width, height, center, zoom) {
   }
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
-      const [a, b] = toComplexAtCenterAndZoom(x,y, width, height, center, zoom)
+      const [a, b] = toComplexAtCenterAndZoom(x, y, width, height, center, zoom)
       const loops = loopMandle([a, b])
-      if (counter % 100000 === 0) console.log('Index', counter)
+      if (counter % 100000 === 0) console.log("Index", counter)
       histo[floor(loops)] += 1
       mData.push(loops)
       counter += 1
@@ -126,43 +137,30 @@ function histoTheData(temps, histo) {
   return hues
 }
 
-function makeScale (colors) {
+function makeScale(colors) {
   let position = 0
   let inc = 1
   const cLength = colors.length
   const items = []
-  const domains = [
-    0,
-    0.25,
-    0.5,
-    0.6,
-    0.7,
-    0.75,
-    0.8,
-    0.82,
-    0.85,
-    0.87,
-    0.91,
-    0.92,
-    0.95,
-    0.99,
-    1
-  ]
-  for(let i = 0; i < 14; i++) {
-    if(position === cLength -1) {
+
+  for (let i = 0; i < COLOR_SHIFTS; i++) {
+    if (position === cLength - 1) {
       inc = -1
     }
-    if(position === 0) {
+    if (position === 0) {
       inc = 1
     }
     items.push(colors[position])
     position += inc
   }
-  items.push('black')
-  return chroma.scale(items).mode('hsl').domain(domains)
+  //items.push("black")
+  return chroma
+    .scale(items)
+    .mode("hsl")
+    .domain(DOMAINS)
 }
 
-function makeScaleCycle (colors) {
+function makeScaleCycle(colors) {
   const cLength = colors.length
   const items = []
   const domains = [
@@ -182,44 +180,31 @@ function makeScaleCycle (colors) {
     0.99,
     1
   ]
-  for(let i = 0; i < 14; i++) {
+  for (let i = 0; i < 14; i++) {
     items.push(colors[i % colors.length])
   }
-  items.push('black')
-  return chroma.scale(items).mode('hsl').domain(domains)
+  items.push("black")
+  return chroma
+    .scale(items)
+    .mode("hsl")
+    .domain(domains)
 }
 
 async function draw() {
-  const lobster = makeScale(['#E24931','#CFB382','#BBE3D8','#8DCFCE','#102A44'])
-  const forest = makeScale(['#22766A','#83C047', '#F0F4CA','#C5A14E','#9F2F29'])
-  const forest2 = makeScaleCycle(['#22766A','#83C047', '#F0F4CA','#C5A14E','#9F2F29'])
-  const fire = makeScale(['white', 'lightyellow', 'yellow', 'red', 'black'])
-  const ocean2 = makeScale(['#ffffff','#d4e2d8','#31d4cb','#33afca','#378abf','#3067ac','#264890'])
-  const ocean3 = makeScaleCycle(['#ffffff','#d4e2d8','#31d4cb','#33afca','#378abf','#3067ac','#264890'])
-  const spectral = makeScale(['#cc0011','#f77105','#ffd600','#e0ff00','#acfe7a','#26e9ff','#007fff'])
   const scales = []
-  while(scales.length < 7) {
-    scales.push(
-      makeRandomScale(
-        ['#ffffff','#d4e2d8','#31d4cb','#33afca','#378abf','#3067ac','#264890'],
-        randomIntFromInterval(4,15)
-      )
-    )
-  }
-  while(scales.length < 14) {
-    scales.push(
-      makeRandomScale(
-        ['#cc0011','#f77105','#ffd600','#e0ff00','#acfe7a','#26e9ff','#007fff'],
-        randomIntFromInterval(4,15)
-      )
-    )
+  for (let i = 56; i < 57; i++) {
+    scales.push(makeScale(color_theme[i]))
   }
 
-  const [temps, histo] = buildMandleData(CANVAS_WIDTH, CANVAS_HEIGHT, CENTER, ZOOM)
+  const [temps, histo] = buildMandleData(
+    CANVAS_WIDTH,
+    CANVAS_HEIGHT,
+    CENTER,
+    ZOOM
+  )
   const histoedData = histoTheData(temps, histo)
-  for(let i = 0; i < scales.length; i++)
-  {
-    const name = `./images/${Date.now()}.png`
+  for (let i = 0; i < scales.length; i++) {
+    const name = `./images/${i}.png`
     const imageResult = await color(scales[i], temps, histoedData)
     const saveResult = await saveImage(imageResult, name)
     console.log(`${name} saved`)
@@ -230,9 +215,9 @@ function color(c_scale, temps, histoedData) {
   return new Promise(function(resolve, reject) {
     gd.createTrueColor(CANVAS_WIDTH, CANVAS_HEIGHT, function(error, image) {
       const white = [255, 255, 255]
-      if (error) reject(error.toString());
-      for (let i = 0; i < histoedData.length; i ++) {
-        if (i % 100000 === 0) console.log('coloring', i)
+      if (error) reject(error.toString())
+      for (let i = 0; i < histoedData.length; i++) {
+        if (i % 100000 === 0) console.log("coloring", i)
         const iters = temps[i]
         const temp = histoedData[i]
         let rgb = c_scale(temp).rgb()
@@ -243,32 +228,31 @@ function color(c_scale, temps, histoedData) {
         if (col > CANVAS_WIDTH - DPI) rgb = white
         if (row < DPI) rgb = white
         if (row > CANVAS_HEIGHT - DPI) rgb = white
-        image.setPixel(col, row, gd.trueColor(rgb[0], rgb[1],rgb[2]))
+        image.setPixel(col, row, gd.trueColor(rgb[0], rgb[1], rgb[2]))
       }
       resolve(image)
-    });
-  });
+    })
+  })
 }
 
-function saveImage (image, name) {
+function saveImage(image, name) {
   return new Promise(function(resolve, reject) {
     image.savePng(name, 0, function(error) {
-
       if (error) reject(error.toString())
       resolve()
-      image.destroy();
+      image.destroy()
     })
-  });
+  })
 }
 
-function toComplexAtCenterAndZoom(x,y,w,h,center,zoom) {
-  let top = center[1] - (zoom * RATIO)
-  let bottom = center[1] + (zoom * RATIO)
+function toComplexAtCenterAndZoom(x, y, w, h, center, zoom) {
+  let top = center[1] - zoom * RATIO
+  let bottom = center[1] + zoom * RATIO
   let left = center[0] - zoom
   let right = center[0] + zoom
   const a = numberMap(x, 0, w, left, right)
   const b = numberMap(y, 0, h, top, bottom)
-  return [a,b]
+  return [a, b]
 }
 
 function hslToRgb(h, s, l) {
@@ -283,7 +267,7 @@ function hslToRgb(h, s, l) {
       if (t < 1 / 6) return p + (q - p) * 6 * t
       if (t < 1 / 2) return q
       if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
-      return p
+      return pc
     }
 
     var q = l < 0.5 ? l * (1 + s) : l + s - l * s
@@ -306,42 +290,42 @@ function numberMap(i, min, max, smin, smax) {
   return p * sDist + smin
 }
 
-function weightAverage (n, w) {
+function weightAverage(n, w) {}
 
-}
-
-
-function makeRandomScale (colors, size) {
+function makeRandomScale(colors, size) {
   const domains = randomDomainList(size)
   let items = fillRandomFrom(colors, size)
   //items[items.length -1] = 'black'
-  const modes = ['lch', 'lab', 'hsl']
-  const mode = modes[randomIntFromInterval(0,2)]
-  console.log('Domains', domains)
-  console.log('Colors', items)
-  console.log('Mode', mode)
-  return chroma.scale(items).mode(mode).correctLightness().domain(domains)
+  const modes = ["lch", "lab", "hsl"]
+  const mode = modes[randomIntFromInterval(0, 2)]
+  console.log("Domains", domains)
+  console.log("Colors", items)
+  console.log("Mode", mode)
+  return chroma
+    .scale(items)
+    .mode(mode)
+    .correctLightness()
+    .domain(domains)
 }
 
-function cycleArray (a) {
+function cycleArray(a) {
   const item0 = a.shift()
   a.push(item0)
   return a
 }
 
-function randomIntFromInterval(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 function getRandom(min, max) {
-  return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min
 }
 
 function fillRandomFrom(original, count) {
   let final = []
   while (final.length < count) {
-    const idx = randomIntFromInterval(0, original.length -1)
+    const idx = randomIntFromInterval(0, original.length - 1)
     final.push(original[idx])
   }
   return final
@@ -351,7 +335,7 @@ function randomDomainList(size) {
   let position = 0.0
   let l = [0.0]
   while (l.length < size - 2) {
-    const  newPosition = getRandom(position, 0.999)
+    const newPosition = getRandom(position, 0.999)
     l.push(newPosition)
     position = newPosition
   }
